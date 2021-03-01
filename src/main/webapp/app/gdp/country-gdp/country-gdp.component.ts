8a7e3af85022652eb56a0b8e7347e412f359f3bb
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-03-01 21:11:18 +0100

    added chart.js and country component ts

--- src/main/webapp/app/gdp/country-gdp/country-gdp.component.ts
+++ src/main/webapp/app/gdp/country-gdp/country-gdp.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IGdpData } from '../../shared/model/chart.gdp.model';
+import { ICountry } from '../../shared/model/country.model';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'jhi-country-gdp',
@@ -6,7 +11,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-gdp.component.scss'],
 })
 export class CountryGDPComponent implements OnInit {
-  constructor() {}
+  currentCountry: ICountry;
+  data: IGdpData[];
+  preGDPUrl: string = 'http://api.worldbank.org/v2/countries/';
+  postGDPUrl: string = '/indicators/NY.GDP.MKTP.CD?format=json&per_page=' + 10;
+  year = [];
+  gdp = [];
+  chart = [];
+  noDataAvailable: any;
 
-  ngOnInit(): void {}
+  constructor(private activatedRoute: ActivatedRoute, private httpClient: HttpClient) {
+    this.activatedRoute.data.subscribe(result => {
+      this.currentCountry = result.country;
+    });
+  }
+
+  ngOnInit(): void {
+    const gdpUrl = this.preGDPUrl + this.currentCountry.code + this.postGDPUrl;
+    this.httpClient.get(gdpUrl).subscribe(res => {
+      this.noDataAvailable = true;
+      const gdpDataArr = res[1];
+      if (gdpDataArr) {
+        this.noDataAvailable = false;
+        gdpDataArr.forEach(item => {
+          this.year.push(item.date);
+          this.gdp.push(item.value);
+        });
+
+        this.year = this.year.reverse();
+        this.gdp = this.gdp.reverse();
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: this.year,
+            datasets: [
+              {
+                data: this.gdp,
+                borderColor: '#3CBA9F',
+                fill: true,
+              },
+            ],
+          },
+          options: {
+            legend: {
+              display: false,
+            },
+            scales: {
+              xAxes: [{ display: true }],
+              yAxes: [{ display: true }],
+            },
+          },
+        });
+      }
+    });
+  }
 }
